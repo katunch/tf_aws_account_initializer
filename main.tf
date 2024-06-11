cda70e79e1e0c5bfeb206f9322660c578da01a12
Silas Knobel <silas.knobel@katun.ch>
2024-06-11 09:17:58 +0200

    remove provider alias

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ terraform {
     aws = {
       source                = "hashicorp/aws"
       version               = "~> 5"
-      configuration_aliases = [aws.default, aws.us-east-1]
     }
   }
 }
